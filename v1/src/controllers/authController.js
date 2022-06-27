8b3ea10199924fdb9dc57eb6f9c6dacbc3847e10
jaleyerdelen <jaleyerdelen@gmail.com>
2022-06-27 23:47:39 +0300

    jwt

--- v1/src/controllers/authController.js
+++ v1/src/controllers/authController.js
@@ -1,5 +1,21 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const sendToken = (user, statusCode, req, res) => {
+  const token = jwt.sign(
+    { id: user._id, name: user.name, isAdmin: user.isAdmin },
+    process.env.JWT_SEC,
+    { expiresIn: process.env.EXPIRES }
+  );
+  res.cookie("jwt", token, { httpOnly: true });
+  const { password, ...others } = user._doc;
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    others,
+  });
+};
 
 const createUser = async (req, res) => {
   try {
@@ -8,10 +24,7 @@ const createUser = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
-    res.status(200).json({
-      message: "created user",
-      user,
-    });
+    sendToken( user, 200, req, res )
   } catch (error) {
     res.status(400).json({
       message: "error creating user",
@@ -27,6 +40,7 @@ const loginUser = async (req, res) => {
     if (!user) return res.status(400).send("email couldn't find");
     const compared = await bcrypt.compare(pass, user.password);
     if (compared === false) return res.status(400).send("wrong password ");
+  if(user)  return sendToken(user, 200, req, res)
     const { password, ...others } = user._doc;
     res.status(200).json({
       message: "success login",
